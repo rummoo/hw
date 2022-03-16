40f927bf5358ea85783661ebaf6760269c528a19
Eren Yılmaz <36042943+rummoo@users.noreply.github.com>
2022-03-17 00:50:24 +0300

    Delete 3.ÖDEV directory

--- 3.ÖDEV/Fonk-2.py
+++
@@ -1,7 +0,0 @@
-from math import exp
-import numpy as np
-df=open('output.txt','w')
-for i in np.arange(0,100,0.01):
-    y = (i*i)*((exp(i)-exp(-i))/2)
-    df.write('        %r        %r\n' % (i,y))
-df.close()
